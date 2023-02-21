840b5318ffd1844a2cc66e4b5fe64e2c59eb997a
dzhuraa <d.zhurakivsky@gmail.com>
2023-02-21 13:22:22 +0100

    initial commit

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import {axiosService} from "../../services";
-import {MoviesListCard} from "../MoviesListCard/MoviesListCard";
+// import React, {useEffect, useState} from 'react';
+// import {axiosService} from "../../services";
+// import {MoviesListCard} from "../MoviesListCard/MoviesListCard";
 
 const MoviesList = () => {
-    const [movies, setMovies] = useState([]);
+    // const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        axiosService.get('/discover').then(({data})=>setMovies([...data]))
-    }, [])
+    // useEffect(() => {
+    //     axiosService.get('/discover').then(({data})=>setMovies([...data]))
+    // }, [])
     return (
         <div>
-            {movies.map(movie=><MoviesListCard key={movie.id} movie={movie}/>)}
+            {/*{movies.map(movie=><MoviesListCard key={movie.id} movie={movie}/>)}*/}
         </div>
     );
 };
